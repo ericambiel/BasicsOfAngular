2ffacfcd001ba500a7d6da9fc6c033b60e9bb41c
Eric Ambiel <eric.ambiel@gmail.com>
2019-07-14 13:06:27 -0300

    Aula 10 Class e Style binding

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 
+//Bootstrap
+import { AlertModule } from 'ngx-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
