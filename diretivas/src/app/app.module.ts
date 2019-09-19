78dbf2732d23ca8a6b0491b008fe9af6059f2a03
Eric Ambiel <eric.ambiel@gmail.com>
2019-09-18 21:29:59 -0300

    Instalação do bootstrap e adição de um componente ngSwitch

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgSwitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiretivaNgifComponent
+    DiretivaNgifComponent,
+    DiretivaNgSwitchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
